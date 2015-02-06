86f8d9ec5770db1b532aa0f9a866574f5c24e915
Danny B <daniel@brasuk.net>
2015-02-06 15:54:39 -0500

    Add tables for Date and Time dimension (but not indexes)

--- EnergyEDW/Schema Objects/dim.sql
+++ EnergyEDW/Schema Objects/dim.sql
@@ -1,2 +1,1 @@
-﻿
-CREATE SCHEMA [dim]
+﻿CREATE SCHEMA [dim]
---
+++ EnergyEDW/Tables/Date.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dim].[Date]
+(
+  [Date_PK] [int] NOT NULL CONSTRAINT PK_Date PRIMARY KEY
+, [FullDate] [datetime] NOT NULL
+, [Day] [tinyint] NOT NULL
+, [DaySuffix] [varchar](4) NOT NULL
+, [DayOfWeek] [varchar](9) NOT NULL
+, [DayOfWeekNumber] [int] NOT NULL
+, [DayOfWeekInMonth] [tinyint] NOT NULL
+, [DayOfYearNumber] [int] NOT NULL
+, [RelativeDays] int NOT NULL
+, [WeekOfYearNumber] [tinyint] NOT NULL
+, [WeekOfMonthNumber] [tinyint] NOT NULL
+, [RelativeWeeks] int NOT NULL
+, [CalendarMonthNumber] [tinyint] NOT NULL
+, [CalendarMonthName] [varchar](9) NOT NULL
+, [CalendarYearMonth] as (case when [CalendarMonthNumber] < 10 then cast ([CalendarYearNumber] as varchar(7)) + '-0' + cast([CalendarMonthNumber] as varchar(7)) else cast ([CalendarYearNumber] as varchar(7)) + '-' + cast([CalendarMonthNumber] as varchar(7))  end)
+, [RelativeMonths] int NOT NULL
+, [CalendarQuarterNumber] [tinyint] NOT NULL
+, [CalendarQuarterName] [varchar](6) NOT NULL
+, [RelativeQuarters] int NOT NULL
+, [CalendarYearNumber] int NOT NULL
+, [RelativeYears] int NOT NULL
+, [StandardDate] [varchar](10) NULL
+, [WeekDayFlag] bit NOT NULL 
+, [HolidayFlag] bit NOT NULL
+, [OpenFlag] bit NOT NULL
+, [FirstDayOfCalendarMonthFlag] bit NOT NULL
+, [LastDayOfCalendarMonthFlag] bit NOT NULL
+, [HolidayText] [varchar](50) NULL
+)
+ON SECONDARY;

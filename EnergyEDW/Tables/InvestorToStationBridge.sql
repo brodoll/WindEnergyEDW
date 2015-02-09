679dc30bac6cf0e24128ea6715a18d7c97cce04d
Danny B <daniel@brasuk.net>
2015-02-09 12:40:39 -0500

    Rename Plant Mainteanance file to MaintenanceEvent, to align with table

--- EnergyEDW/Tables/InvestorToStationBridge.sql
+++ EnergyEDW/Tables/InvestorToStationBridge.sql
@@ -6,10 +6,13 @@ StationFK INTEGER NOT NULL,
 InvolvementInInvestmentUSDollars DECIMAL(11,0)  NULL,
 InvolvementInInvestmentPercent DECIMAL(5,2)  NULL,
 InvestmentStatusType NVARCHAR(254) NOT NULL,
-InvestmentEffectiveDateLocalFK INT  NULL,
-InvestmentExpirationDateLocalFK INT  NULL,
+[InvestmentEffectiveDateFK] INT  NULL,
+[InvestmentExpirationDateFK] INT  NULL,
 CurrentInvestmentRecordFlag BIT NOT NULL,
-LastUpdateDate DATETIME NOT NULL CONSTRAINT DF_InvestorToStationBridge_LastUpdateDate DEFAULT Current_TimeStamp
+LastUpdateDate DATETIME NOT NULL CONSTRAINT DF_InvestorToStationBridge_LastUpdateDate DEFAULT Current_TimeStamp, 
+    CONSTRAINT [FK_InvestorToStationBridge_ToInvolvedParty] FOREIGN KEY ([InvestorFK]) REFERENCES [dim].[InvolvedPartyEntity]([InvolvedPartyEntityPK]), 
+    CONSTRAINT [FK_InvestorToStationBridge_ToStation] FOREIGN KEY ([StationFK]) REFERENCES [dim].[Station]([StationPK]), 
+    CONSTRAINT [FK_InvestorToStationBridge_ToDate] FOREIGN KEY ([InvestmentEffectiveDateFK]) REFERENCES [dim].[Date]([DatePK])
 )
 ON [SECONDARY];
 
@@ -24,5 +27,20 @@ GO
 
 CREATE INDEX [IX_InvestorToStationBridge_Station] ON [dim].[InvestorToStationBridge] (StationFK)
 INCLUDE (InvestorFK, InvolvementInInvestmentUSDollars, InvolvementInInvestmentPercent, CurrentInvestmentRecordFlag)
+WITH (FILLFACTOR = 90, PAD_INDEX = ON)
+ON IndexFileGroup;
+GO
+
+CREATE INDEX [IX_InvestorToStationBridge_InvestmentEffectiveDateFK] 
+ON [dim].[InvestorToStationBridge] ([InvestmentEffectiveDateFK])
+INCLUDE (InvestorFK, StationFK, InvolvementInInvestmentUSDollars, InvolvementInInvestmentPercent, CurrentInvestmentRecordFlag)
+WITH (FILLFACTOR = 90, PAD_INDEX = ON)
+ON IndexFileGroup;
+
+GO
+
+CREATE INDEX [IX_InvestorToStationBridge_InvestmentExpirationDateFK] 
+ON [dim].[InvestorToStationBridge] ([InvestmentExpirationDateFK])
+INCLUDE (InvestorFK, StationFK, InvolvementInInvestmentUSDollars, InvolvementInInvestmentPercent, CurrentInvestmentRecordFlag)
 WITH (FILLFACTOR = 90, PAD_INDEX = ON)
 ON IndexFileGroup;

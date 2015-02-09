d52aad80fff5ee1f06be401e5b565a52bc4c80d0
Danny B <daniel@brasuk.net>
2015-02-09 11:47:59 -0500

    Add FKs on Date

--- EnergyEDW/Tables/PlantMaintenanceEvent.sql
+++ EnergyEDW/Tables/PlantMaintenanceEvent.sql
@@ -12,12 +12,34 @@ MaintenanceDurationInDays INT NULL,
 MaintenanceDurationInHours AS (CASE WHEN MaintenanceDurationInDays IS NOT NULL THEN CAST(ROUND(CAST(MaintenanceDurationInDays AS FLOAT) / 24.0,0) AS INTEGER) ELSE NULL END),
 AppliedToFactAfterTheFactFlag BIT NOT NULL,
 DisplayName AS (ScheduleType + '-'+ StandardName),
-LastUpdateDate DATETIME NOT NULL CONSTRAINT DF_MaintenanceEvent_LastUpdateDate DEFAULT Current_TimeStamp
+LastUpdateDate DATETIME NOT NULL CONSTRAINT DF_MaintenanceEvent_LastUpdateDate DEFAULT Current_TimeStamp, 
+    CONSTRAINT [FK_MaintenanceEvent_ToDate_ForBegan] FOREIGN KEY ([BeganOnDateFK]) REFERENCES [dim].[Date]([DatePK]), 
+    CONSTRAINT [FK_MaintenanceEvent_ToDate_ForEnd] FOREIGN KEY ([EndedOnDateFK]) REFERENCES [dim].[Date]([DatePK]), 
+    CONSTRAINT [FK_MaintenanceEvent_ToDate_ForRequested] FOREIGN KEY ([MaintenanceRequestedDateFK]) REFERENCES [dim].[Date]([DatePK])
 )
 ON [SECONDARY];
 GO
 
 CREATE INDEX [IX_MaintenanceEvent_ScheduleType] ON [dim].[MaintenanceEvent] ([ScheduleType])
-INCLUDE (MaintenanceDurationInHours, MaintenanceRequestedUniversalDateFK, BeganOnDateniversalDateFK, EndedOnDateUniversalDateFK)
+INCLUDE (MaintenanceDurationInHours, MaintenanceRequestedDateFK, BeganOnDateFK, EndedOnDateFK)
+WITH (FILLFACTOR = 90, PAD_INDEX = ON)
+ON IndexFileGroup;
+
+GO
+
+CREATE INDEX [IX_MaintenanceEvent_BeganOnDate] ON [dim].[MaintenanceEvent] ([BeganOnDateFK])
+INCLUDE ([ScheduleType])
+WITH (FILLFACTOR = 90, PAD_INDEX = ON)
+ON IndexFileGroup;
+GO
+
+CREATE INDEX [IX_MaintenanceEvent_EndedOnDate] ON [dim].[MaintenanceEvent] ([EndedOnDateFK])
+INCLUDE ([ScheduleType])
+WITH (FILLFACTOR = 90, PAD_INDEX = ON)
+ON IndexFileGroup;
+GO
+
+CREATE INDEX [IX_MaintenanceEvent_MaintenanceRequestedDate] ON [dim].[MaintenanceEvent] ([MaintenanceRequestedDateFK])
+INCLUDE ([ScheduleType])
 WITH (FILLFACTOR = 90, PAD_INDEX = ON)
 ON IndexFileGroup;
